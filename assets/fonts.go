68ca8d047f818c95fc4e833a28c60794f192373d
Iskander Sharipov <quasilyte@gmail.com>
2023-09-30 02:31:04 +0400

    register a font (with approx sizes)

---
+++ assets/fonts.go
@@ -0,0 +1,25 @@
+package assets
+
+import (
+	resource "github.com/quasilyte/ebitengine-resource"
+	"github.com/quasilyte/ge"
+)
+
+func registerFontResources(ctx *ge.Context) {
+	fontResources := map[resource.FontID]resource.FontInfo{
+		FontNormal: {Path: "fonts/whiterabbit.ttf", Size: 18},
+		FontBig:    {Path: "fonts/whiterabbit.ttf", Size: 22},
+	}
+
+	for id, res := range fontResources {
+		ctx.Loader.FontRegistry.Set(id, res)
+		ctx.Loader.LoadFont(id)
+	}
+}
+
+const (
+	FontNone resource.FontID = iota
+
+	FontNormal
+	FontBig
+)
--- cmd/game/main.go
+++ cmd/game/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"time"
 
+	"github.com/quasilyte/cavebots-game/assets"
 	"github.com/quasilyte/cavebots-game/scenes"
 	"github.com/quasilyte/ge"
 )
@@ -15,6 +16,9 @@ func main() {
 	ctx.WindowWidth = 1920
 	ctx.WindowHeight = 1080
 	ctx.FullScreen = true
+
+	ctx.Loader.OpenAssetFunc = assets.MakeOpenAssetFunc(ctx)
+	assets.RegisterResources(ctx)
 
 	if err := ge.RunGame(ctx, scenes.NewMainMenuController()); err != nil {
 		panic(err)
